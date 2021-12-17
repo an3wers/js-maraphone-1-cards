b8c3df82d4ab7c679f99fc1cfaed6d1cd89d4b6f
unknown <user@notebook1.corp.santur.ru>
2021-12-17 10:53:14 +0500

    added function sladePlugin

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-// Получаем массив слайдов
-const slides = document.querySelectorAll('.slide')
+function sladePlugin(activeSlide = 0) {
+    // Получаем массив слайдов
+    const slides = document.querySelectorAll('.slide')
+    slides[activeSlide].classList.add('active')
+    // Перебираем все слайды и по клику изначально убираем со всем эелемнтов класс active, а к нажатаму слайду добавляем
+    for (let s of slides) {
+        s.addEventListener('click', () => {
+            clearActiveClasses()
+            s.classList.add('active')
+        })
+    }
 
-// Перебираем все слайды и по клику изначально убираем со всем эелемнтов класс active, а к нажатаму слайду добавляем
-for (let s of slides) {
-    s.addEventListener('click', () => {
-        clearActiveClasses()
-        s.classList.add('active')
-    })
+    // Функция, которая убирает со всем слайдов класс Active
+    function clearActiveClasses() {
+        slides.forEach(s => {
+            s.classList.remove('active')
+        })
+    }
 }
 
-// Функция, которая убирает со всем слайдов класс Active
-function clearActiveClasses() {
-    slides.forEach(s => {
-        s.classList.remove('active')
-    })
-}
+sladePlugin(1)
